fe9261d32cdf44924c810c6fcefe96d5ad12887a
network <88440862+networktraffic@users.noreply.github.com>
2022-02-25 15:53:16 -0500

    Update src.lua

--- src.lua
+++ src.lua
@@ -2,7 +2,7 @@ local handler_interface = Instance.new('ScreenGui', gethui and gethui( ) or game
 local container = Instance.new( 'Frame', handler_interface )
 local layout = Instance.new( 'UIListLayout', container )
 local padding = Instance.new( 'UIPadding', container )
-local text_object = Instance.new( 'TextLabel', container )
+local text_obj = Instance.new( 'TextLabel', container )
 local render_stepped = game:GetService( 'RunService' ).RenderStepped
 
 handler_interface.Name = math.random( )
@@ -27,56 +27,56 @@ padding.PaddingLeft = UDim.new( 0.25, 0 )
 padding.PaddingRight = UDim.new( 0.25, 0 )
 padding.PaddingTop = UDim.new( 0.25, 0 )
 
-text_object.Name = math.random( )
-text_object.AnchorPoint = Vector2.new( 0.5, 0.5 )
-text_object.BackgroundTransparency = 1
-text_object.BorderSizePixel = 0
-text_object.Position = UDim2.new( 0.5, 0, 0.5, 0 )
-text_object.Size = UDim2.new( 1, 0, 0, text_object.TextBounds.Y )
-text_object.Font = Enum.Font.Gotham
-text_object.TextColor3 = Color3.new( 1, 1, 1 )
-text_object.TextSize = 20
-text_object.TextStrokeTransparency = 0.5
+text_obj.Name = math.random( )
+text_obj.AnchorPoint = Vector2.new( 0.5, 0.5 )
+text_obj.BackgroundTransparency = 1
+text_obj.BorderSizePixel = 0
+text_obj.Position = UDim2.new( 0.5, 0, 0.5, 0 )
+text_obj.Size = UDim2.new( 1, 0, 0, text_obj.TextBounds.Y )
+text_obj.Font = Enum.Font.Gotham
+text_obj.TextColor3 = Color3.new( 1, 1, 1 )
+text_obj.TextSize = 20
+text_obj.TextStrokeTransparency = 0.5
 
-local job_queue, connection = { }, nil
+local job_queue, conn = { }, nil
 do
     local function on_render_stepped( )
-        local index = 0
+        local idx = 0
         local current_job = job_queue[ 1 ]
 
         if current_job then
-            connection:Disconnect( )
+            conn:Disconnect( )
 
             local text = current_job[ 1 ]
             local time_alive = current_job[ 2 ]
             local type_speed = current_job[ 3 ]
 
             if text and time_alive and type_speed then
-                text_object.Text = text
+                text_obj.Text = text
                 text = string.gsub( text, '<br%s*/>', '\n' )
                 text = string.gsub( text, '<[^<>]->', '' )
 
                 for _ in utf8.graphemes( text ) do
-                    index += 1
-                    text_object.MaxVisibleGraphemes = index
+                    idx += 1
+                    text_obj.MaxVisibleGraphemes = idx
                     task.wait( type_speed )
                 end
 
                 task.wait( time_alive )
 
                 for _ in utf8.graphemes( text ) do
-                    index -= 1
-                    text_object.MaxVisibleGraphemes = index
+                    idx -= 1
+                    text_obj.MaxVisibleGraphemes = idx
                     task.wait( type_speed )
                 end
                 
                 table.remove( job_queue, 1 )
-                connection = render_stepped:Connect( on_render_stepped )
+                conn = render_stepped:Connect( on_render_stepped )
             end
         end
     end
 
-    connection = render_stepped:Connect( on_render_stepped )
+    conn = render_stepped:Connect( on_render_stepped )
 end
 
 return function( text, time_alive, type_speed )
